49335a26623e25308f1780d0084e2f2b13d38dbb
sivasrikanth96 <sivasrikanth96@gmail.com>
2025-04-12 15:55:36 +0530

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,1 +1,11 @@
 
+let formContainer = document.getElementById("submitForm");
+let taskTitleElement = document.getElementById("taskTitle")
+
+let tasksList = [];
+
+formContainer.addEventListener("submit", function(event) {
+    event.preventDefault();
+    taskTitleElement.value = "";
+
+})
